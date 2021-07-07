ce7e4c7486caa07146eb2a0094da70c078572824
Estrella <estrella.cholod@splight-ae.com>
2021-07-07 11:27:46 -0300

    SDEV-123-RefreshLogin add refresh login

--- src/api/publicFetch.js
+++ src/api/publicFetch.js
@@ -1,5 +1,9 @@
 import axios from "axios";
-import { httpRequestsValues } from "../constants/api.constants";
+import {
+  httpRequestsValues,
+  platform_API_URL,
+  REFRESH,
+} from "../constants/api.constants";
 
 const { POST, DELETE, GET, PUT } = httpRequestsValues;
 
@@ -14,22 +18,50 @@ const actionFnIsOk = (action) =>
  */
 const genericHttpRequest = async (action, endpoint, params = {}, API_URL) => {
   const endpointIsOk = !!endpoint && typeof endpoint === "string";
-  const user = sessionStorage.getItem("user");
+  let user = sessionStorage.getItem("user");
   const headers = !!user && {
     Authorization: "Bearer " + JSON.parse(user).access,
   };
-  const isHeaders = !!user ? { params, headers } : params;
   const actionIsOk = actionFnIsOk(action);
   const paramsOk = endpointIsOk && actionIsOk;
   const publicFetch = axios.create({
     baseURL: API_URL,
   });
 
+  const loginRefresh = async () => {
+    const params = { refresh: JSON.parse(user).refresh };
+    try {
+      const { data } = await axios.post(platform_API_URL + REFRESH, params);
+      const newUser = {
+        ...JSON.parse(user),
+        access: data.access,
+      };
+      sessionStorage.setItem("user", JSON.stringify(newUser));
+      return {
+        Authorization: "Bearer " + newUser.access,
+      };
+    } catch (error) {
+      console.log("error :>> ", error);
+    }
+  };
+
   if (paramsOk) {
     try {
-      const { data } = await publicFetch[action](endpoint, isHeaders);
+      const { data } = await publicFetch[action](
+        endpoint,
+        !!user ? { params, headers } : params
+      );
       return data;
     } catch (error) {
+      if (error.response.status === 401) {
+        const headers = await loginRefresh();
+
+        const { data } = await publicFetch[action](endpoint, {
+          params,
+          headers,
+        });
+        return data;
+      }
       throw error;
     }
   }
