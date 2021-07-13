ece46fc2a5c758961aaf649df3a0d2be5330a14f
Estrella <estrella.cholod@splight-ae.com>
2021-07-13 16:39:01 -0300

    SDEV-114-BackUpScreen test

---
+++ src/components/DPM/BackUp/backUp.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Typography, TextField, InputAdornment } from "@material-ui/core";
+import "./index.css";
+
+const BackUpComponent = (props) => {
+  return <div className="backUp"></div>;
+};
+
+export default BackUpComponent;
---
+++ src/components/DPM/BackUp/backUp.container.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react";
+import BackUpComponent from "./backUp.component";
+
+const BackUp = (props) => {
+  return <BackUpComponent />;
+};
+
+export default BackUp;
