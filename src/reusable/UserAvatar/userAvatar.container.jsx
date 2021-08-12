7f1f9c873f0dd7a4088c0317f663772599646d66
Estrella <estrella.cholod@splight-ae.com>
2021-08-12 14:49:54 -0300

    SDEV-144-InstanceMEnu add logout

---
+++ src/reusable/UserAvatar/userAvatar.container.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UserAvatarComponent from "./userAvatar.component";
+
+const UserAvatar = ({ user }) => {
+  return <UserAvatarComponent user={user} />;
+};
+
+export default UserAvatar;
