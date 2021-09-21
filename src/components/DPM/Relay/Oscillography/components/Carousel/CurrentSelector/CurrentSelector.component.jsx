23b0fe76c17490f69dfaafafef2625df5967f215
Estrella <estrella.cholod@splight-ae.com>
2021-09-21 16:35:07 -0300

    SDEV-204-bis deleted !important

--- src/components/DPM/Relay/Oscillography/components/Carousel/CurrentSelector/CurrentSelector.component.jsx
+++ src/components/DPM/Relay/Oscillography/components/Carousel/CurrentSelector/CurrentSelector.component.jsx
@@ -1,33 +1,35 @@
-import React, { useEffect } from 'react'
-import { Button, InputLabel, MenuItem, Select, Chip } from '@material-ui/core'
-import './CurrentSelector.css'
-
+import React, { useEffect } from "react";
+import { Button, InputLabel, MenuItem, Select, Chip } from "@material-ui/core";
+import "./CurrentSelector.css";
 
 const CurrentSelectorComponent = (props) => {
-    
+  useEffect(() => {}, [props]);
 
-    useEffect(() => {
-        
-    }, [props])
-
-
-
-
-    return (props.currentGraphData && <div className='current_selector_container'>
-        <Select value={1} className='carousel_header_select' onChange={props.handleCurrentSelect}>
-            <MenuItem value={1}>
-                <em>Señales</em>
-            </MenuItem>
-            {
-                props.createRenderArray()
-            }
+  return (
+    props.currentGraphData && (
+      <div className="current_selector_container">
+        <Select
+          value={1}
+          className="carousel_header_select"
+          onChange={props.handleCurrentSelect}
+        >
+          <MenuItem value={1}>
+            <em>Señales</em>
+          </MenuItem>
+          {props.createRenderArray()}
         </Select>
-        <div className={props.hideDetails ? 'chip_scrollable_container_extended': 'chip_scrollable_container'}>
-            {
-                props.createChipsArray()
-            }
-        </div></div>
+        <div
+          className={
+            props.hideDetails
+              ? "chip_scrollable_container_extended"
+              : "chip_scrollable_container"
+          }
+        >
+          {props.createChipsArray()}
+        </div>
+      </div>
     )
-}
+  );
+};
 
-export default CurrentSelectorComponent
+export default CurrentSelectorComponent;
